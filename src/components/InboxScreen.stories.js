c678d9a5934a12498e45f4c77dc3effb7ccc2e71
yichangmin <changmin.yi@myrealtrip.com>
2022-10-17 14:55:08 +0900

    InboxScreen Play 제거

--- src/components/InboxScreen.stories.js
+++ src/components/InboxScreen.stories.js
@@ -25,14 +25,14 @@ Default.parameters = {
   },
 };
 
-Default.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement);
-  await waitForElementToBeRemoved(await canvas.findByTestId('loading'));
-  await waitFor(async () => {
-    await fireEvent.click(canvas.getByLabelText('pinTask-2'));
-    await fireEvent.click(canvas.getByLabelText('pinTask-3'));
-  });
-};
+// Default.play = async ({ canvasElement }) => {
+//   const canvas = within(canvasElement);
+//   await waitForElementToBeRemoved(await canvas.findByTestId('loading'));
+//   await waitFor(async () => {
+//     await fireEvent.click(canvas.getByLabelText('pinTask-2'));
+//     await fireEvent.click(canvas.getByLabelText('pinTask-3'));
+//   });
+// };
 
 export const Error = Template.bind({});
 Error.parameters = {
